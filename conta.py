a05f7afd1ae47c6a2b7d877ee5fd04785781cdaf
Tiago Mazzon <tiago.mazzon@gmail.com>
2022-10-24 11:55:22 -0300

    getters, setters, métodos estáticos, métodos privados

--- conta.py
+++ conta.py
@@ -1,5 +1,4 @@
 
-
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
@@ -8,6 +7,7 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        #self.__codigo_banco = "001"
 
     def extrato(self):
         print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
@@ -15,9 +15,37 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.saldo + self.limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou do limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+        print("Limite modificado para {}".format(limite))
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
